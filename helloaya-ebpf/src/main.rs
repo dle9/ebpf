7fdd280b9c69edf352c53234e0f5f874041d3c21
Deric Le <dericle77@gmail.com>
2025-02-14 12:08:31 -0600

    nice

--- helloaya-ebpf/src/main.rs
+++ helloaya-ebpf/src/main.rs
@@ -1,9 +1,57 @@
 #![no_std]
 #![no_main]
+#![allow(nonstandard_style, dead_code)]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+/*
+ * XDP is a type of eBPF program that attaches to a network interface;
+ * network packet -> user XDP program -> network driver/linux kernel
+ *
+ * XDP action determines what happens to the packet: XDP_{PASS, DROP, ABORTED, TX, REDIRECT}
+ *
+ * XDP context provides access to the network packet
+ */
+use aya_ebpf::{
+    bindings::xdp_action,
+    macros::{map, xdp},
+    maps::HashMap,
+    programs::XdpContext,
+};
 use aya_log_ebpf::info;
 
+// for parsing the network packet
+use core::mem;
+use network_types::{
+    eth::EthHdr,
+    ip::{IpProto, Ipv4Hdr},
+    tcp::TcpHdr,
+    udp::UdpHdr,
+};
+
+#[map]
+static BLOCKLIST: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(1024, 0);
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// before accessing packet data, insert bound checks required by ebpf verifier
+// gets a pointer to start and end of the packet
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+// the xdp program entrypoint
 #[xdp]
 pub fn helloaya(ctx: XdpContext) -> u32 {
     match try_helloaya(ctx) {
@@ -12,13 +60,42 @@ pub fn helloaya(ctx: XdpContext) -> u32 {
     }
 }
 
-fn try_helloaya(ctx: XdpContext) -> Result<u32, u32> {
-    info!(&ctx, "received a packet");
+fn try_helloaya(ctx: XdpContext) -> Result<u32, ()> {
+    let (source_ip, source_port) = extract_source(&ctx)?;
+
+    let action = if block_ip(source_ip) {
+        xdp_action::XDP_DROP
+    } else {
+        xdp_action::XDP_PASS
+    };
+
+    info!(&ctx, "{}, {:i}:{}", action, source_ip, source_port);
+
     Ok(xdp_action::XDP_PASS)
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+fn block_ip(address: u32) -> bool {
+    unsafe { BLOCKLIST.get(&address).is_some() }
+}
+
+fn extract_source(ctx: &XdpContext) -> Result<(u32, u16), ()> {
+    // pass the offset, EthHdr, to skip EthHdr and go to IP hdr
+    let ipv4hdr: *const Ipv4Hdr = ptr_at(ctx, EthHdr::LEN)?;
+
+    // from big endian to target endian
+    let source_ip = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+
+    let source_port = match unsafe { (*ipv4hdr).proto } {
+        IpProto::Tcp => {
+            let tcphdr: *const TcpHdr = ptr_at(ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be(unsafe { (*tcphdr).source })
+        }
+        IpProto::Udp => {
+            let udphdr: *const UdpHdr = ptr_at(ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be(unsafe { (*udphdr).source })
+        }
+        _ => return Err(()),
+    };
+
+    Ok((source_ip, source_port))
 }
--- helloaya/src/main.rs
+++ helloaya/src/main.rs
@@ -1,8 +1,12 @@
-use anyhow::Context as _;
-use aya::programs::{Xdp, XdpFlags};
+use anyhow::Context;
+use aya::{
+    maps::HashMap,
+    programs::{Xdp, XdpFlags},
+};
+use aya_log::EbpfLogger;
 use clap::Parser;
-#[rustfmt::skip]
-use log::{debug, warn};
+use log::{info, warn};
+use std::net::Ipv4Addr;
 use tokio::signal;
 
 #[derive(Debug, Parser)]
@@ -12,44 +16,46 @@ struct Opt {
 }
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
-
     env_logger::init();
 
-    // Bump the memlock rlimit. This is needed for older kernels that don't use the
-    // new memcg based accounting, see https://lwn.net/Articles/837122/
-    let rlim = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
-    };
-    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    if ret != 0 {
-        debug!("remove limit on locked memory failed, ret is: {}", ret);
-    }
-
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
-    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+    // loads the file into the compiled binary, runs instantly
+    // use Ebpf::load_file, to run from a file instead
+    let mut bpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
         env!("OUT_DIR"),
         "/helloaya"
     )))?;
-    if let Err(e) = aya_log::EbpfLogger::init(&mut ebpf) {
-        // This can happen if you remove all log statements from your eBPF program.
+
+    // boilerplate
+    if let Err(e) = EbpfLogger::init(&mut bpf) {
         warn!("failed to initialize eBPF logger: {}", e);
     }
-    let Opt { iface } = opt;
-    let program: &mut Xdp = ebpf.program_mut("helloaya").unwrap().try_into()?;
+
+    let program: &mut Xdp = bpf.program_mut("helloaya").unwrap().try_into()?;
     program.load()?;
-    program.attach(&iface, XdpFlags::SKB_MODE)
+    program.attach(&opt.iface, XdpFlags::default())
         .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
-    let ctrl_c = signal::ctrl_c();
-    println!("Waiting for Ctrl-C...");
-    ctrl_c.await?;
-    println!("Exiting...");
+    // populate the blocklist hashmap in the ebpf file
+    let mut blocklist_dst: HashMap<_, u32, u32> =
+        HashMap::try_from(bpf.map_mut("BLOCKLIST").unwrap())?;
+
+    let mut ips: Vec<(u8, u8, u8, u8)> = Vec::new();
+    ips.push((10, 246, 208, 1));
+    let blocklist_src: Vec<u32> = ips
+        .iter()
+        .map(|&(a, b, c, d)| Ipv4Addr::new(a, b, c, d).into())
+        .collect();
+
+    for addr in blocklist_src {
+        blocklist_dst.insert(addr, 0, 0)?;
+    }
+
+    // boilerplate
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
 
     Ok(())
 }
